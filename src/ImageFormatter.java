9bb12afa5a66dfa7ed2c1e4f1cc03fa600f29669
Veronica Xue <johnnysangel@163.com>
2016-01-19 14:05:03 +0800

    Rotate the image

--- src/ImageFormatter.java
+++ src/ImageFormatter.java
@@ -3,6 +3,8 @@ import java.awt.Component;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 import java.awt.image.FilteredImageSource;
 import java.awt.image.ImageFilter;
@@ -22,8 +24,11 @@ public class ImageFormatter {
         BufferedImage bfImage = null;
         System.out.println(Math.sqrt(9));
         try {
-            image = ImageIO.read(new File("/Users/veronica/Desktop/material/png/invoice9.png") );
-            //bfImage = ImageIO.read(new File("/Users/veronica/Desktop/invoice4.png") );
+            //image = ImageIO.read(new File("/Users/veronica/Desktop/material/png/invoice9.png") );
+        	 image = ImageIO.read(new File("/Users/veronica/Desktop/test.tif") );
+
+
+
           
             ImageFilter colorfilter = new ColorComponentScaler();             
             FilteredImageSource filteredImageSource = new FilteredImageSource(image.getSource(), colorfilter );
@@ -50,14 +55,26 @@ public class ImageFormatter {
            FilteredImageSource filteredNewImageSource = new FilteredImageSource(filteredImage.getSource(), blackFilter);
            Image filteredNewImage = Toolkit.getDefaultToolkit().createImage(filteredNewImageSource);
            BufferedImage newFiltered = toBufferedImage(filteredNewImage);
+           newFiltered = rotate90DX(newFiltered);
 
-            save(newFiltered, "png");    
+
+            save(newFiltered, "tif");    
         } catch( IOException e){
             //do something
             System.out.print(e.getMessage());
         }
     }
 
+	
+	public static BufferedImage rotate90DX(BufferedImage bi) {
+	    int width = bi.getWidth();
+	    int height = bi.getHeight();
+	    BufferedImage biFlip = new BufferedImage(height, width, bi.getType());
+	    for(int i=0; i<width; i++)
+	        for(int j=0; j<height; j++)
+	            biFlip.setRGB(height-1-j, i, bi.getRGB(i, j));
+	    return biFlip;
+	}
 	
 	public static BufferedImage toBufferedImage(Image img)
 	{
@@ -79,7 +96,7 @@ public class ImageFormatter {
 	}
 	
     private static void save(BufferedImage image, String ext) {
-        String fileName = "generated9";
+        String fileName = "test2";
         File file = new File("/Users/veronica/Desktop/" + fileName + "." + ext);
         try {
             ImageIO.write(image, ext, file);  // ignore returned boolean
